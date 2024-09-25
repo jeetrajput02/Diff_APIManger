b9d8b9d6a0f289c84c7c0cccd361a1d44fe8be5f
jeetrajput02 <73092375+jeetrajput02@users.noreply.github.com>
2024-09-25 18:33:32 +0530

    add sample code

---
+++ sample/sample/Model/BrandModel.swift
@@ -0,0 +1,15 @@
+//
+//  BrandModel.swift
+//  Observation Framework
+//
+//  Created by differenz53 on 08/07/24.
+//
+
+import Foundation
+
+//MARK: - BrandModel
+struct brandModel: Codable {
+    var id: Int?
+    var uid, brand, equipment: String?
+}
+
---
+++ sample/sample/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  sample
+//
+//  Created by differenz53 on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
